cdaf5754b348fc4bbf95708f4de4d746828ab485
Rob_R <1743138+Robbot@users.noreply.github.com>
2021-05-30 20:35:45 +0100

    Snake game improved with text file storing results

--- Days_01-25/Day24/Snake Game Improved/scoreboard.py
+++ Days_01-25/Day24/Snake Game Improved/scoreboard.py
@@ -1,16 +1,21 @@
 from turtle import Turtle
+
 ALIGMENT = "center"
 FONT = ("Ariel", 24, "normal")
+
+
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("data.txt") as f:
+            self.high_score = int(f.read())
         self.color("white")
         self.penup()
         self.goto(0, 265)
         self.hideturtle()
         self.update_scoreboard()
+
 
     def update_scoreboard(self):
         self.clear()
@@ -19,17 +24,12 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", "w") as f:
+                f.write(f"{self.high_score}")
+
         self.score = 0
         self.update_scoreboard()
 
     def increase_score(self):
         self.score += 1
         self.update_scoreboard()
-
-
-
-
-
-
-
-
