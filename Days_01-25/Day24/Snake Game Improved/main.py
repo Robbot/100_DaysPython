dba573713450452c45b4c41816012ca0d859eba8
Rob_R <1743138+Robbot@users.noreply.github.com>
2021-05-30 19:22:10 +0100

    Snake game improved

---
+++ Days_01-25/Day24/Snake Game Improved/main.py
@@ -0,0 +1,70 @@
+from snake import Snake
+from scoreboard import Scoreboard
+import time
+from food import Food
+from turtle import Screen
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(snake.up,"Up")
+screen.onkey(snake.down,"Down")
+screen.onkey(snake.left,"Left")
+screen.onkey(snake.right, "Right")
+
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
+
+
+    #Detect colision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    #Detect collision with wall
+    if snake.head.xcor() > 288  or snake.head.xcor() < -288 or snake.head.ycor() > 288 or snake.head.ycor() < -288:
+        scoreboard.reset()
+        snake.reset()
+
+    #Detect collision with tail
+    for segment in snake.segments:
+        if segment == snake.head:
+                pass
+        elif snake.head.distance(segment) < 10:
+            scoreboard.reset()
+            snake.reset()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
