d412deb5654d1d41767c4e2feffd8b63a3e5f14e
Rob_R <1743138+Robbot@users.noreply.github.com>
2021-06-09 12:08:54 +0100

    day 25

---
+++ Days_01-25/Day25/Weather/main.py
@@ -0,0 +1,16 @@
+# with open("weather_data.csv") as data_file:
+#     data = data_file.readlines()
+#     print(data)
+# import csv
+import pandas
+#
+# with open("weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temperatures = []
+#     for row in data:
+#         if row[1] != 'temp':
+#             temperatures.append(int(row[1]))
+#     print(temperatures)
+#
+data = pandas.read_csv("weather_data.csv")
+print(data["temp"])
