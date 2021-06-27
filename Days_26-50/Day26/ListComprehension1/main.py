315b8678d452774b3badebce9281aa5abfded835
Rob_R <1743138+Robbot@users.noreply.github.com>
2021-06-27 22:25:38 +0100

    List comprehensions

---
+++ Days_26-50/Day26/ListComprehension1/main.py
@@ -0,0 +1,15 @@
+new_range = [x*2 for x in range(1,5)]
+print(new_range)
+
+
+numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+# 🚨 Do Not Change the code above 👆
+
+#First *fork* your copy. Then copy-paste your code below this line 👇
+#Finally click "Run" to execute the tests
+
+squared_numbers = [number*number for number in numbers]
+
+#Write your code 👆 above:
+
+print(squared_numbers)
