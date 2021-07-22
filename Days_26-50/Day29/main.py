fdb4d39c5ba829025f216f39fb6b108dcd006b12
Rob_R <1743138+Robbot@users.noreply.github.com>
2021-07-22 22:28:01 +0100

    Password Manager finished

--- Days_26-50/Day29/main.py
+++ Days_26-50/Day29/main.py
@@ -1,45 +1,79 @@
 from tkinter import *
-import math
+from tkinter import messagebox
+from random import choice, randint, shuffle
+import pyperclip
 
-FONT_NAME = "Ariel"
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
-    pass
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = randint(8, 10)
+    nr_symbols = randint(2, 4)
+    nr_numbers = randint(2, 4)
+
+    password_letters = [choice(letters) for _ in range(nr_letters)]
+    password_symbols = [choice(symbols) for _ in range(nr_symbols)]
+    password_numbers = [choice(numbers) for _ in range(nr_numbers)]
+
+    password_list =  password_letters + password_numbers + password_symbols
+    shuffle(password_list)
+    password = "".join(password_list)
+    pass_entry.insert(0, password)
+    pyperclip.copy(password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
-def add_password():
-    pass
+def save():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = pass_entry.get()
 
-
+    if len(website) < 1 or len(email) < 1 or len(password) < 1:
+        messagebox.showinfo(title="Incomplete data", message="You forgot to enter all data!")
+    else:
+        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email}"
+                                                              f"\nPassword: {password} \nIs it ok to save?")
+        if is_ok:
+            with open("data.txt", "a") as data_entry:
+                data_entry.write(f"{website} | {email} | {password} \n")
+                website_entry.delete(0, 'end')
+                email_entry.delete(0, 'end')
+                email_entry.insert(0, "email@server.com")
+                pass_entry.delete(0, 'end')
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
 window.title("Password Manager")
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=50)
 
-canvas = Canvas(width=200, height=224)
+canvas = Canvas(width=200, height=200)
 logo_img = PhotoImage(file="logo.png")
-canvas.create_image(100, 112, image=logo_img)
+canvas.create_image(100, 100, image=logo_img)
 canvas.grid(column=1, row=0)
 
-website_label = Label(text="Website: ", font=(FONT_NAME, 10))
-website_label.grid(column=0, row=1, sticky=E)
-email_label = Label(text="Email/Usernaname: ", font=(FONT_NAME, 10))
-email_label.grid(column=0, row=2,sticky=E)
-password_label = Label(text="Password:", font=(FONT_NAME, 10))
-password_label.grid(column=0, row=3,)
+website_label = Label(text="Website: ")
+website_label.grid(column=0, row=1)
+email_label = Label(text="Email/Usernaname: ")
+email_label.grid(column=0, row=2)
+password_label = Label(text="Password:")
+password_label.grid(column=0, row=3)
 
-button1 = Button(text="Generate Password", command=generate_password, highlightthickness=0)
-button1.grid(column=2, row=3)
-button2 = Button(text="Add", command=add_password, highlightthickness=0, width=36)
-button2.grid(column=1, row=4, columnspan=2)
+generate_password_button = Button(text="Generate Password", command=generate_password, highlightthickness=0)
+generate_password_button.grid(column=2, row=3)
+add_button = Button(text="Add", command=save, highlightthickness=0, width=36)
+add_button.grid(column=1, row=4, columnspan=2)
 
 website_entry = Entry(width=35)
-website_entry.grid(column=1, row=1,  columnspan=2)
+website_entry.grid(column=1, row=1, columnspan=2)
+website_entry.focus()
 email_entry = Entry(width=35)
 email_entry.grid(column=1, row=2, columnspan=2)
-pass_entry = Entry(width=17)
+email_entry.insert(0, "email@server.com")
+pass_entry = Entry(width=21)
 pass_entry.grid(column=1, row=3)
 
 window.mainloop()
