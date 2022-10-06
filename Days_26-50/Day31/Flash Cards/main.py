d454d9ccad3be5ef56bbb7b6f4245a6c87f70915
Robbot <verwolff1@gmail.com>
2022-10-07 00:31:28 +0100

    Flash cards project finished

--- Days_26-50/Day31/Flash Cards/main.py
+++ Days_26-50/Day31/Flash Cards/main.py
@@ -2,9 +2,18 @@ import random
 from tkinter import *
 import pandas
 
-data = pandas.read_csv("data/es-en_1000_common_words.csv")
-to_learn = data.to_dict(orient="records")
+BACKGROUND_COLOR = "#B1DDC6"
 current_card = {}
+to_learn = {}
+
+try:
+    data = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("data/es-en_1000_common_words.csv")
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
+
 
 def next_card():
     global current_card, flip_timer
@@ -20,15 +29,16 @@ def flip_card():
     canvas.itemconfig(card_title, text="English translation", fill="white")
     canvas.itemconfig(card_word, text=current_card["English"], fill="white")
     canvas.itemconfig(card_background, image=card_back_image)
-    
+
 
 def is_known():
     to_learn.remove(current_card)
     next_card()
-    
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("data/words_to_learn.csv", index=False)
 
 # ------------------------------- UI SETUP --------------------------------- #
-BACKGROUND_COLOR = "#B1DDC6"
+
 
 window = Tk()
 window.title("Flashy")
@@ -52,7 +62,7 @@ unknown_button.grid(row=1, column=0)
 check_image = PhotoImage(file="images/right.png")
 known_button = Button(image=check_image, highlightthickness=0, command=is_known)
 known_button.grid(row=1, column=1)
-print(len(to_learn))
+
 next_card()
 
 window.mainloop()
