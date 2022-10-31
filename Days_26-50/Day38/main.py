e2f38df52116d93baa96ac6e051f0ca716598c14
Robbot <1743138+Robbot@users.noreply.github.com>
2022-10-31 21:44:41 +0000

    rebuilding with config.ini

--- Days_26-50/Day38/main.py
+++ Days_26-50/Day38/main.py
@@ -1,29 +1,27 @@
+import configparser
 import requests
 from datetime import datetime
 import os
 
-db_user = os.environ.get("DB_USER")
-db_password = os.environ.get("DB_PASS")
 
-print(db_user)
-print(db_password)
+config = configparser.ConfigParser()
+config.read('config.ini')
 
-
-GENDER = YOUR GENDER
-WEIGHT_KG = YOUR WEIGHT
-HEIGHT_CM = YOUR HEIGHT
-AGE = YOUR AGE
-
-APP_ID = os.environ["NT_APP_ID"]
-API_KEY = os.environ["NT_API_KEY"]
+GENDER = str(config['constants']['gender'])
+WEIGHT_KG = str(config['constants']['weight'])
+HEIGHT_CM = str(config['constants']['height'])
+AGE = str(config['constants']['age'])
 
 exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
-sheet_endpoint = os.environ["SHEET_ENDPOINT"]
+# sheet_endpoint = os.environ["SHEET_ENDPOINT"]
 
 exercise_text = input("Tell me which exercises you did: ")
-
+API_ID = str(config['nutritionix']['api_id'])
+print(API_ID)
+API_KEY = str(config['nutritionix']['api_key'])
+print(API_KEY)
 headers = {
-    "x-app-id": APP_ID,
+    "x-app-id": API_ID,
     "x-app-key": API_KEY,
 }
 
@@ -37,25 +35,26 @@ parameters = {
 
 response = requests.post(exercise_endpoint, json=parameters, headers=headers)
 result = response.json()
+print(result)
 
-today_date = datetime.now().strftime("%d/%m/%Y")
-now_time = datetime.now().strftime("%X")
-
-bearer_headers = {
-    "Authorization": f"Bearer {os.environ['TOKEN']}"
-}
-
-for exercise in result["exercises"]:
-    sheet_inputs = {
-        "workout": {
-            "date": today_date,
-            "time": now_time,
-            "exercise": exercise["name"].title(),
-            "duration": exercise["duration_min"],
-            "calories": exercise["nf_calories"]
-        }
-    }
-
-    sheet_response = requests.post(sheet_endpoint, json=sheet_inputs, headers=bearer_headers)
-
-    print(sheet_response.text)
+# today_date = datetime.now().strftime("%d/%m/%Y")
+# now_time = datetime.now().strftime("%X")
+#
+# bearer_headers = {
+#     "Authorization": f"Bearer {os.environ['TOKEN']}"
+# }
+#
+# for exercise in result["exercises"]:
+#     sheet_inputs = {
+#         "workout": {
+#             "date": today_date,
+#             "time": now_time,
+#             "exercise": exercise["name"].title(),
+#             "duration": exercise["duration_min"],
+#             "calories": exercise["nf_calories"]
+#         }
+#     }
+#
+#     sheet_response = requests.post(sheet_endpoint, json=sheet_inputs, headers=bearer_headers)
+#
+#     print(sheet_response.text)
