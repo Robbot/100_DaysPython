bd05cc0c4c370a5282a7218933ed19833a5f1463
Robbot <1743138+Robbot@users.noreply.github.com>
2022-11-01 17:23:00 +0000

    rebuilding with config.ini finished

--- Days_26-50/Day38/main.py
+++ Days_26-50/Day38/main.py
@@ -13,13 +13,11 @@ HEIGHT_CM = str(config['constants']['height'])
 AGE = str(config['constants']['age'])
 
 exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
-# sheet_endpoint = os.environ["SHEET_ENDPOINT"]
+sheet_endpoint = str(config['sheety']['SHEET_ENDPOINT'])
 
 exercise_text = input("Tell me which exercises you did: ")
 API_ID = str(config['nutritionix']['api_id'])
-print(API_ID)
 API_KEY = str(config['nutritionix']['api_key'])
-print(API_KEY)
 headers = {
     "x-app-id": API_ID,
     "x-app-key": API_KEY,
@@ -37,24 +35,27 @@ response = requests.post(exercise_endpoint, json=parameters, headers=headers)
 result = response.json()
 print(result)
 
-# today_date = datetime.now().strftime("%d/%m/%Y")
-# now_time = datetime.now().strftime("%X")
-#
-# bearer_headers = {
-#     "Authorization": f"Bearer {os.environ['TOKEN']}"
-# }
-#
-# for exercise in result["exercises"]:
-#     sheet_inputs = {
-#         "workout": {
-#             "date": today_date,
-#             "time": now_time,
-#             "exercise": exercise["name"].title(),
-#             "duration": exercise["duration_min"],
-#             "calories": exercise["nf_calories"]
-#         }
-#     }
-#
-#     sheet_response = requests.post(sheet_endpoint, json=sheet_inputs, headers=bearer_headers)
-#
-#     print(sheet_response.text)
+today_date = datetime.now().strftime("%d/%m/%Y")
+now_time = datetime.now().strftime("%X")
+
+token = str(config['sheety']['TOKEN'])
+print(token)
+
+bearer_headers = {
+     "Authorization": f"Bearer {token}"
+ }
+
+for exercise in result["exercises"]:
+    sheet_inputs = {
+        "workout": {
+             "date": today_date,
+             "time": now_time,
+             "exercise": exercise["name"].title(),
+             "duration": exercise["duration_min"],
+             "calories": exercise["nf_calories"]
+         }
+     }
+
+    sheet_response = requests.post(sheet_endpoint, json=sheet_inputs, headers=bearer_headers)
+
+    print(sheet_response.text)
