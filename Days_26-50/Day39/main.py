1691aa358733a8b3a17924aa92eb3ab8668e5d08
Robbot <1743138+Robbot@users.noreply.github.com>
2022-11-04 23:33:06 +0000

    Day39, Flight search engine done.

--- Days_26-50/Day39/main.py
+++ Days_26-50/Day39/main.py
@@ -1,10 +1,19 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
 # 4. Pass the data back to the main.py file, so that you can print the data from main.py
+from datetime import datetime, timedelta
 from data_manager import DataManager
-data_manager = DataManager()
-sheet_data = data_manager.get_destination_data()
-# print(sheet_data)
+from flight_search import FlightSearch
+from notification_manager import NotificationManager
 
+ORIGIN_CITY_IATA = "LON"
+
+data_manager = DataManager()
+notification_manager = NotificationManager()
+flight_search = FlightSearch()
+
+sheet_data = data_manager.get_destination_data()
+
+# print(sheet_data)
 #  5. In main.py check if sheet_data contains any values for the "iataCode" key.
 #  If not, then the IATA Codes column is empty in the Google Sheet.
 #  In this case, pass each city name in sheet_data one-by-one
@@ -16,7 +25,21 @@ if sheet_data[0]["iataCode"] == "":
     flight_search = FlightSearch()
     for row in sheet_data:
         row["iataCode"] = flight_search.get_destination_code(row["city"])
-    print(f"sheet_data:\n {sheet_data}")
-
     data_manager.destination_data = sheet_data
     data_manager.update_destination_codes()
+    
+tomorrow = datetime.now() + timedelta(days=1)
+six_month_from_today = datetime.now() + timedelta(days=(6 * 30))
+    
+for destination in sheet_data:
+    flight = flight_search.check_flights(
+        ORIGIN_CITY_IATA,
+        destination["iataCode"],
+        from_time=tomorrow,
+        to_time=six_month_from_today
+    )
+
+    if flight.price < destination["lowestPrice"]:
+        notification_manager.send_sms(
+            message=f"Low price alert! Only £{flight.price} to fly from {flight.origin_city}-{flight.origin_airport} to {flight.destination_city}-{flight.destination_airport}, from {flight.out_date} to {flight.return_date}."
+        )
